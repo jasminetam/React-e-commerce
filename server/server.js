3e105b6638eba73ca7a885f492b1fbe64d09ab7f
jasminetam <jasminetam94@gmail.com>
2022-06-05 23:01:10 +0100

    modifying functions

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");
-
-const app = express()
-
-app.get("/", (req, res) => {
-    res.send("API is Running...")
-})
-
-app.listen(3000,console.log("server running..."))
